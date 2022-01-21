bf92aa28d941e6dacb96cbabfd714298b79561c4
saurabhchidrewar <saurabhchidrewar5@gmail.com>
2022-01-21 19:02:37 +0530

    Completed till Performance charts

--- src/Content.js
+++ src/Content.js
@@ -1,6 +1,7 @@
-import filter from "./assets/filter.png";
 import React from "react";
+import filter from "./assets/filter.png";
 import calendar from "./assets/calendar.png";
+import { Line } from "react-chartjs-2";
 
 const Content = () => {
   return (
@@ -72,12 +73,227 @@ const Content = () => {
         </div>
       </div>
 
-      <div className="keyMatrics">
-        <div className="keyMatricsContainer">
-          <p></p>
+      <div className="keyMetricsHeading">KEY METRICS</div>
+      <div className="wrap">
+        <div className="cell-wrap-left">
+          <table className="leftKeyMetrics">
+            <tr>
+              <th>Total Tyres Deployed</th>
+              <th>Total Fleets Engaged</th>
+              <th>Avg Tyre Life Value</th>
+              <th>Avg TCO/Km</th>
+            </tr>
+            <tr>
+              <td>243k</td>
+              <td>12k</td>
+              <td>
+                63k <span className="keyMetricsInnerData">kms</span>{" "}
+              </td>
+              <td>
+                {" "}
+                <span className="keyMetricsInnerData">₹</span> 2.1
+              </td>
+            </tr>
+          </table>
         </div>
-        <div className="keyMatricsContainer"></div>
-        <p></p>
+        <div className="cell-wrap-right">
+          <table className="rightKeyMetrics">
+            <tr>
+              <th>% Uptime</th>
+              <th>Mean Time Between Failure</th>
+              <th>Mean Time To Repair</th>
+              <th>Avg Tyre Pressure</th>
+            </tr>
+            <tr>
+              <td>97.7%</td>
+              <td>
+                42.6<span className="keyMetricsInnerData">days</span>{" "}
+              </td>
+              <td>
+                1.7<span className="keyMetricsInnerData">hrs</span>{" "}
+              </td>
+              <td>36</td>
+            </tr>
+          </table>
+        </div>
+      </div>
+
+      <div className="performanceHeading">PERFORMANCE</div>
+      <div className="performance">
+        <div className="performanceChart">
+          <div>
+            <Line
+              data={{
+                labels: ["Jun", "Jul", "Aug"],
+                datasets: [
+                  {
+                    data: [20, 60, 40],
+                    fill: false,
+                    borderColor: "#0077b6",
+                    borderWidth: 1,
+                    pointRadius: 1,
+                  },
+                ],
+              }}
+              height={150}
+              width={150}
+              options={{
+                title: {
+                  display: true,
+                  text: "Active vs Inactive Vehicles",
+                  position: 'bottom',
+                  fontColor: "#0077b6",
+                  fontSize: 10,
+                },
+                maintainAspectRatio: true,
+                scales: {
+                  yAxes: [
+                    {
+                      gridLines: {
+                        display: false,
+                      },
+                      ticks: {
+                        beginAtZero: true,
+                        stepSize: 20,
+                        fontSize: 8,
+                        suggestedMax: 100,
+                      },
+                    },
+                  ],
+                  xAxes: [
+                    {
+                      gridLines: {
+                        display: false,
+                      },
+                      ticks: {
+                        fontSize: 8,
+                      },
+                    },
+                  ],
+                },
+                legend: {
+                  display: false,
+                },
+              }}
+            />
+          </div>
+        </div>
+        <div className="performanceChart">
+          <div>
+            <Line
+              data={{
+                labels: ["Jun", "Jul", "Aug"],
+                datasets: [
+                  {
+                    data: [20, 40, 25],
+                    fill: false,
+                    borderColor: "#f72585",
+                    borderWidth: 1,
+                    pointRadius: 1,
+                  },
+                ],
+              }}
+              height={150}
+              width={150}
+              options={{
+                title: {
+                  display: true,
+                  text: "Cost per km vs Total Cost",
+                  position: 'bottom',
+                  fontColor: "#f72585",
+                  fontSize: 10,
+                },
+                maintainAspectRatio: true,
+                scales: {
+                  yAxes: [
+                    {
+                      gridLines: {
+                        display: false,
+                      },
+                      ticks: {
+                        suggestedMax: 100,
+                        beginAtZero: true,
+                        stepSize: 20,
+                        fontSize: 8,
+                      },
+                    },
+                  ],
+                  xAxes: [
+                    {
+                      gridLines: {
+                        display: false,
+                      },
+                      ticks: {
+                        fontSize: 8,
+                      },
+                    },
+                  ],
+                },
+                legend: {
+                  display: false,
+                },
+              }}
+            />
+          </div>
+        </div>
+        <div className="performanceChart">
+          <div>
+            <Line
+              data={{
+                labels: ["Jun", "Jul", "Aug"],
+                datasets: [
+                  {
+                    data: [12, 40, 100],
+                    fill: false,
+                    borderColor: "#0077b6",
+                    borderWidth: 1,
+                    pointRadius: 1,
+                  },
+                ],
+              }}
+              height={150}
+              width={150}
+              options={{
+                title: {
+                  display: true,
+                  text: "Active vs Inactive Vehicles",
+                  position: 'bottom',
+                  fontColor: "#0077b6",
+                  fontSize: 10,
+                },
+                maintainAspectRatio: true,
+                scales: {
+                  yAxes: [
+                    {
+                      gridLines: {
+                        display: false,
+                      },
+                      ticks: {
+                        suggestedMax: 100,
+                        beginAtZero: true,
+                        stepSize: 20,
+                        fontSize: 8,
+                      },
+                    },
+                  ],
+                  xAxes: [
+                    {
+                      gridLines: {
+                        display: false,
+                      },
+                      ticks: {
+                        fontSize: 8,
+                      },
+                    },
+                  ],
+                },
+                legend: {
+                  display: false,
+                },
+              }}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
